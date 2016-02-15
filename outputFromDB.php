04ea4663f364a477e8ad1ace0142b5ed5a09fb0d
CyrilKrasnobaiev <kirill.krasnobaiev@gmail.com>
2016-02-15 17:40:54 +0200

    Deleted username and password

--- outputFromDB.php
+++ outputFromDB.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "babay2008";
-$dbname = "sku";
+$servername = "----";
+$username = "----";
+$password = "-----";
+$dbname = "------";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
